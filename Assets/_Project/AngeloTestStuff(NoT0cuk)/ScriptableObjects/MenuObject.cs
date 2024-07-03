8593ffa4cb70c129c03f1a57968bfd48a09e8eb8
Angelo-SAE <a.allaham@student.saedubai.com>
2024-07-03 07:49:04 +0400

    Started working on cooking mechanic

--- Assets/_Project/AngeloTestStuff(NoT0cuk)/ScriptableObjects/MenuObject.cs
+++ Assets/_Project/AngeloTestStuff(NoT0cuk)/ScriptableObjects/MenuObject.cs
@@ -8,4 +8,11 @@ public class MenuObject : ScriptableObject
     public string[] orderTag;
     public float[] orderPrice;
     public GameObject[] orderObject;
+    public FoodNumber[] orderIngredients;
+}
+
+[System.Serializable]
+public struct FoodNumber
+{
+  public int[] foodNumber;
 }
