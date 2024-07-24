dec57884ac184f1f41740658abaae0775e330e31
Khalid Himmo <khalidhimmo07@gmail.com>
2024-07-24 10:35:33 +0400

    Fixed UI freaking out

--- Assets/_Project/Scripts/UI Scripts/ButtonHoverEffect.cs
+++ Assets/_Project/Scripts/UI Scripts/ButtonHoverEffect.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ButtonHoverEffect : MonoBehaviour
 {
     [SerializeField] private RectTransform uiToScale;
     [SerializeField] private Vector3 hoverScale;
+    [SerializeField] private float verticalCast, horizontalCast;
     private RectTransform rectTransform;
     private Vector3 originalScale;
     private AudioSource audioSource;
+    private bool isInside;
+
 
     private void Start()
     {
         originalScale = uiToScale.localScale;
         audioSource = GetComponent<AudioSource>();
     }
-
-    public void OnPointerEnter(PointerEventData eventData)
+    private void Update()
     {
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(mouseWorldPos.x > transform.position.x - horizontalCast && mouseWorldPos.x < transform.position.x + horizontalCast && mouseWorldPos.y > transform.position.y - verticalCast && mouseWorldPos.y < transform.position.y + verticalCast)
+        {
+            if(!isInside)
+            {
+                OnPointerEnter();
+            }
+        } else if(isInside)
+        {
+            OnPointerExit();
+        }
+    }
+
+    public void OnPointerEnter()
+    {
+        isInside = true;
         uiToScale.localScale = hoverScale;
 
         if (audioSource != null)
@@ -28,8 +46,9 @@ public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerEx
         }
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    public void OnPointerExit()
     {
+        isInside = false;
         uiToScale.localScale = originalScale;
     }
 }
