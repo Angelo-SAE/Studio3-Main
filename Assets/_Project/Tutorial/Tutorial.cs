bd9cd53d619229ae15af00561cf811157b82f5b1
Saif <empty0smile775@gmail.com>
2024-08-18 13:44:47 +0400

    Tutorial Stage #2 almost done

--- Assets/_Project/Tutorial/Tutorial.cs
+++ Assets/_Project/Tutorial/Tutorial.cs
@@ -9,27 +9,37 @@ public class Tutorial : MonoBehaviour
     int numberTimes = 0;
     
     public GameObject thePlayer;
+    public GameObject grandfather;
+    public GameObject customerCounter;
     
     public GameObject mainCamera;
     public GameObject tutorialCamera;
+    public GameObject kitchenCamera;
     
     public GameObject playerUI;
     public GameObject notesUI;
     public GameObject blackScreen;
     public GameObject leaveTutorial;
+    public GameObject startBasics;
+    public GameObject startCooking;
 
+    public PlayableDirector stage2;
 
 
     // Start is called before the first frame update
     void Start()
     {
         thePlayer.GetComponent<PlayerMovement>().enabled = false;
+        customerCounter.GetComponent<InteractEvent>().enabled = false;
         playerUI.SetActive(false);
         //mainCamera.SetActive(false);
         //tutorialCamera.SetActive(true);
         notesUI.SetActive(false);
         blackScreen.SetActive(true);
         leaveTutorial.SetActive(false);
+        startBasics.SetActive(false);
+        kitchenCamera.SetActive(false);
+        startCooking.SetActive(false); 
     }
 
     public void StartMoving()
@@ -39,6 +49,31 @@ public class Tutorial : MonoBehaviour
         tutorialCamera.SetActive(false);
         playerUI.SetActive(true);
         leaveTutorial.SetActive(false);
+        startBasics.SetActive(false);
+    }
+
+    public void StartBasic()
+    {
+        thePlayer.GetComponent<PlayerMovement>().enabled = false;
+        playerUI.SetActive(false);
+        startBasics.SetActive(true);
+
+    }
+
+    public void StartCookingBasic()
+    {
+        thePlayer.GetComponent<PlayerMovement>().enabled = true;
+        grandfather.layer = LayerMask.NameToLayer("Default");
+        kitchenCamera.SetActive(true);
+        mainCamera.SetActive(false);
+        tutorialCamera.SetActive(false);
+        playerUI.SetActive(true);
+    }
+
+    public void BasicTraining()
+    {
+        stage2.Play();
+        startBasics.SetActive(false);
     }
 
     public void OpenNote()
@@ -47,7 +82,7 @@ public class Tutorial : MonoBehaviour
     }
     public void CloseNote()
     {
-        //stage2.Play();
+
         StartMoving();
         notesUI.SetActive(false);
     }
@@ -74,6 +109,10 @@ public class Tutorial : MonoBehaviour
         if (Input.anyKey && notesUI.activeSelf)
         {
             CloseNote();
+        }
+       if(startCooking.activeSelf)
+        {
+            StartCookingBasic();
         }
     }
 }
